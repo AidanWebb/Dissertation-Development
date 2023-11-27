c21fd378926cde35358b19593aa4c0c549a4b074
Aidan Webb <aidan@ultra-horizon.com>
2023-11-27 00:46:43 +0000

    Flask set up for signup and login pages

--- src/blueprints/website/root_pages.py
+++ src/blueprints/website/root_pages.py
@@ -6,7 +6,19 @@ from . import website
 @website.route('/')
 def home():
     """
-    Returns the homepage for UH VPN.
+    Returns the homepage for Project.
     """
     return render_template('index.html')
 
+@website.route('/login')
+def login():
+    """
+    Returns the login page for project where existing users can sign in.
+    """
+    return render_template('login.html')
+@website.route('/signup')
+def signup():
+    """
+    Returns the sign up page for new users to create an account.
+    """
+    return render_template('signup.html')
