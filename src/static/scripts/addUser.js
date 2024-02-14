ecde32b9a64d6a107974645c0ba0e995372ad19c
Aidan Webb <aidan@ultra-horizon.com>
2024-02-14 13:08:07 +0000

    adjustments to add user

--- src/static/scripts/addUser.js
+++ src/static/scripts/addUser.js
@@ -1,9 +1,41 @@
- $(document).ready(function() {
-            // Function to toggle search bar
-            function toggleSearchBar() {
-                $("#searchBar").toggle();
-            }
+$(document).ready(function() {
+    // Function to toggle search bar
+    function toggleSearchBar() {
+        $("#searchBar").toggle();
+    }
 
-            // Bind click event to add button
-            $("#addButton").click(toggleSearchBar);
+
+    function searchUser() {
+        var searchInput = $("#searchInput").val().trim();
+
+
+        if (searchInput === "") {
+
+            console.log('Please enter a username.');
+            return;
+        }
+
+
+        $.ajax({
+            type: 'POST',
+            url: '/search_user',
+            data: { username: searchInput },
+            success: function(response) {
+                console.log('Search successful:', response);
+            },
+            error: function(xhr, status, error) {
+                console.error('Error searching for user:', error);
+            }
         });
+    }
+
+    // Bind click event to add button
+    $("#addButton").click(function() {
+        toggleSearchBar();
+    });
+
+    // Bind click event to search button
+    $("#searchButton").click(function() {
+        searchUser();
+    });
+});
